44f506effaa5a10e8fd11e7238c04664b394f8ab
Zach Peagler <123001207+zachpeagler@users.noreply.github.com>
2024-10-28 16:23:01 -0400

    Create multifitPlotly.R
    Create functions to plot PDFs and CDFs.

---
+++ multifitPlotly.R
@@ -0,0 +1,127 @@
+#' Multiple PDF Plot For Continuous Variables
+#' Using Plotly
+#'
+#' This function returns a plotly output showing the PDFs for selected distributions
+#' against a continuous, non-negative input variable. Possible distributions include "normal",
+#' "lognormal", "gamma", "exponential", "cauchy", "t", "weibull", "logistic",
+#' and "all".
+#'
+#' @param x The variable to for which to plot PDFs
+#' @param seq_length The number of points over which to fit x
+#' @param distributions The distributions to fit x against
+#' @returns A dataframe with x, the real density, and the pdf of the desired
+#'  distributions with length(nrow) equal to seq_length +1.
+#' @export
+multiPDF_plotly <- function (x, seq_length, distributions) {
+  # check if "all" was passed to distributions
+  if ("all" %in% distributions) {
+    distributions <- c("normal",
+                       "lognormal",
+                       "gamma",
+                       "exponential",
+                       "cauchy",
+                       "t",
+                       "weibull",
+                       "logistic")
+  }
+  # calculate PDFs
+  data <- multiPDF_cont(x, seq_length, distributions)
+  # create plot with real density
+  p <- plotly::plot_ly(data, x= ~x_seq, y= ~dens, type = 'scatter', mode = 'lines', name = 'Actual Density')
+  # check for each type of distribution in the distributions, and add it if present
+  if ("normal" %in% distributions == TRUE) {
+    p <- p %>% plotly::add_trace(p, y=~pdf_normal, name='Normal')
+  }
+  if ("lognormal" %in% distributions == TRUE) {
+    p <- p %>% plotly::add_trace(p, y=~pdf_lognormal, name='Lognormal')
+  }
+  if ("gamma" %in% distributions == TRUE) {
+    p <- p %>% plotly::add_trace(p, y=~pdf_gamma, name='Gamma')
+  }
+  if ("exponential" %in% distributions == TRUE) {
+    p <- p %>% plotly::add_trace(p, y=~pdf_exponential, name='Exponential')
+  }
+  if ("cauchy" %in% distributions == TRUE) {
+    p <- p %>% plotly::add_trace(p, y=~pdf_cauchy, name='Cauchy')
+  }
+  if ("t" %in% distributions == TRUE) {
+    p <- p %>% plotly::add_trace(p, y=~pdf_t, name='t')
+  }
+  if ("weibull" %in% distributions == TRUE) {
+    p <- p %>% plotly::add_trace(p, y=~pdf_weibull, name='Weibull')
+  }
+  if ("logistic" %in% distributions == TRUE) {
+    p <- p %>% plotly::add_trace(p, y=~pdf_logistic, name='LogLogistic')
+  }
+  p <- p %>% plotly::layout(legend = list(orientation = "h",
+                                          xanchor = "center",
+                                          x = 0.5,
+                                          yanchor = "top",
+                                          y = -0.2,
+                                          title = list(text = "Distribution")))
+  return(p)
+}
+
+#' Multiple CDF Plot For Continuous Variables
+#' Using Plotly
+#'
+#' This function returns a plotly output showing the CDFs for selected distributions
+#' against a continuous, non-negative input variable. Possible distributions include "normal",
+#' "lognormal", "gamma", "exponential", "cauchy", "t", "weibull", "logistic",
+#' and "all".
+#'
+#' @param x The variable to for which to plot PDFs
+#' @param seq_length The length over which to fit x
+#' @param distributions The distributions to fit x against
+#' @returns A dataframe with x, the real density, and the pdf of the desired
+#'  distributions with length(nrow) equal to seq_length +1.
+#' @export
+multiCDF_plotly <- function (x, seq_length, distributions) {
+  # check if "all" was passed to distributions
+  if ("all" %in% distributions) {
+    distributions <- c("normal",
+                       "lognormal",
+                       "gamma",
+                       "exponential",
+                       "cauchy",
+                       "t",
+                       "weibull",
+                       "logistic")
+  }
+  # calculate PDFs
+  data <- multiCDF_cont(x, seq_length, distributions)
+  # create plot with real density
+  p <- plotly::plot_ly(data, x= ~x_seq, y= ~dens, type = 'scatter', mode = 'lines', name = 'Actual Distribution')
+  # check for each type of distribution in the distributions, and add it if present
+  if ("normal" %in% distributions == TRUE) {
+    p <- p %>% plotly::add_trace(p, y=~cdf_normal, name='Normal')
+  }
+  if ("lognormal" %in% distributions == TRUE) {
+    p <- p %>% plotly::add_trace(p, y=~cdf_lognormal, name='Lognormal')
+  }
+  if ("gamma" %in% distributions == TRUE) {
+    p <- p %>% plotly::add_trace(p, y=~cdf_gamma, name='Gamma')
+  }
+  if ("exponential" %in% distributions == TRUE) {
+    p <- p %>% plotly::add_trace(p, y=~cdf_exponential, name='Exponential')
+  }
+  if ("cauchy" %in% distributions == TRUE) {
+    p <- p %>% plotly::add_trace(p, y=~cdf_cauchy, name='Cauchy')
+  }
+  if ("t" %in% distributions == TRUE) {
+    p <- p %>% plotly::add_trace(p, y=~cdf_t, name='t')
+  }
+  if ("weibull" %in% distributions == TRUE) {
+    p <- p %>% plotly::add_trace(p, y=~cdf_weibull, name='Weibull')
+  }
+  if ("logistic" %in% distributions == TRUE) {
+    p <- p %>% plotly::add_trace(p, y=~cdf_logistic, name='LogLogistic')
+  }
+  p <- p %>% plotly::layout(legend = list(orientation = "h",
+                                          xanchor = "center",
+                                          x = 0.5,
+                                          yanchor = "top",
+                                          y = -0.2,
+                                          title = list(text = "Distribution")))
+  return(p)
+}
